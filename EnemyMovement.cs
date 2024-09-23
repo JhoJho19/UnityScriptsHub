ba33971a377f80f788c4ba65d644d07c22617f95
JhoJho19 <132712062+JhoJho19@users.noreply.github.com>
2024-09-23 20:39:28 +0300

    Create EnemyMovement.cs

---
+++ EnemyMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private Collider2D colliderForDestroy;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] bool movingRight = true;
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 force = Vector2.zero;
+
+        if (movingRight)
+        {
+            force = Vector2.right;
+        }
+        else
+        {
+            force = Vector2.left;
+        }
+
+        rb.AddForce(force * speed * Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+
+    public void Flip()
+    {
+        movingRight = !movingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnemyFlipper"))
+        {
+            Flip();
+        }
+    }
+}
